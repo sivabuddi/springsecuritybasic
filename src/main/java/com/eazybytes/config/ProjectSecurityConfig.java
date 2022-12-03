d688bb08d00ed4b53bb61fee0f795e2e6b13ce75
sivakumar <sivakumarcareer.buddi@gmail.com>
2022-12-03 11:24:28 -0600

    Configured for customd services and validating username and password for more than one user.

--- src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // responsible for configuration and create beans for those classes mentioned in this class.
@@ -14,29 +17,25 @@ public class ProjectSecurityConfig {
     @Order(SecurityProperties.BASIC_AUTH_ORDER)
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().permitAll()
-                .and().httpBasic()
-                .and().formLogin();
-        return http.build();
-    }
-
-//   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .anyRequest().denyAll()
-//                .and().httpBasic()
-//                .and().formLogin();
-//        return http.build();
-//   }
-
-    // Below is the custom security configurations.
-
-    /*SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
                 .antMatchers("/myaccount","/mybalance","/myloans","/mycards").authenticated()
                 .antMatchers("/mynotices","/mycontacts").permitAll()
                 .and().httpBasic()
                 .and().formLogin();
                 return http.build();
-    }*/
+    }
+    @Bean
+    public InMemoryUserDetailsManager useDetailService(){
+
+        UserDetails admin = User.withDefaultPasswordEncoder().
+                username("admin").password("12345").authorities("admin").build();
+
+        UserDetails user= User.withDefaultPasswordEncoder().
+                username("user").password("12345").authorities("read").build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+
+
+    }
+
 
 }
