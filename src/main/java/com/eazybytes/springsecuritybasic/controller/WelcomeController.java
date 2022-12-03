65922d75dcaefd603edbf83b01426f77886ecfec
sivakumar <sivakumarcareer.buddi@gmail.com>
2022-12-02 19:28:00 -0600

    Welcome getmapping

--- src/main/java/com/eazybytes/springsecuritybasic/controller/WelcomeController.java
+++ src/main/java/com/eazybytes/springsecuritybasic/controller/WelcomeController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
     @GetMapping("/welcome")
     public String welcome(){
-        return "Welcome to Spring Boot without security";
+        return "Welcome to Spring Boot With security";
     }
 }
