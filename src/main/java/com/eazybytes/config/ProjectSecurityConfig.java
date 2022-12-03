f51a5077d56b9a07cc6b51f56d2e7c0e53dbc2eb
sivakumar <sivakumarcareer.buddi@gmail.com>
2022-12-03 11:51:12 -0600

    Configured for customd services and validating username and password for more than one user approach2.

--- src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -26,16 +28,33 @@ public class ProjectSecurityConfig {
     @Bean
     public InMemoryUserDetailsManager useDetailService(){
 
-        UserDetails admin = User.withDefaultPasswordEncoder().
-                username("admin").password("12345").authorities("admin").build();
+        // Approach 1
 
-        UserDetails user= User.withDefaultPasswordEncoder().
-                username("user").password("12345").authorities("read").build();
+//        UserDetails admin = User.withDefaultPasswordEncoder().
+//                username("admin").password("12345").authorities("admin").build();
+//
+//        UserDetails user= User.withDefaultPasswordEncoder().
+//                username("user").password("12345").authorities("read").build();
+//
+//        return new InMemoryUserDetailsManager(admin, user);
 
-        return new InMemoryUserDetailsManager(admin, user);
 
 
+        // Approach 2
+
+
+
+        InMemoryUserDetailsManager inMemoryUserDetailsManager= new InMemoryUserDetailsManager();
+        UserDetails admin = User.withUsername("admin").password("12345").authorities("admin").build();
+        UserDetails user = User.withUsername("user").password("12345").authorities("user").build();
+        inMemoryUserDetailsManager.createUser(admin);
+        inMemoryUserDetailsManager.createUser(user);
+        return inMemoryUserDetailsManager;
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
 
 }
