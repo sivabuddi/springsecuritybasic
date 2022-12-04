7cefc542725bd432731761ad1775a5ab1fd81ce5
sivakumar <sivakumarcareer.buddi@gmail.com>
2022-12-03 18:56:26 -0600

    Database connections with spring security

--- src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -5,19 +5,46 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 @Configuration // responsible for configuration and create beans for those classes mentioned in this class.
+@EnableWebSecurity
 public class ProjectSecurityConfig {
 
     @Bean
     @Order(SecurityProperties.BASIC_AUTH_ORDER)
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().permitAll()
+                .antMatchers("/myaccount","/mybalance","/myloans","/mycards").authenticated()
+                .antMatchers("/mynotices","/mycontacts").permitAll()
                 .and().httpBasic()
                 .and().formLogin();
-        return http.build();
+                return http.build();
+    }
+//    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .anyRequest().permitAll()
+//                .and().httpBasic()
+//                .and().formLogin();
+//        return http.build();
+//    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 
 //   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
