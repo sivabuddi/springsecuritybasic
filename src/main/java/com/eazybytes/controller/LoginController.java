87dfcbcd8febd5bb271f3558bc1afb5e799c2727
sivakumar <sivakumarcareer.buddi@gmail.com>
2022-12-04 18:11:23 -0600

    updated try block using passwordEncoder

--- src/main/java/com/eazybytes/controller/LoginController.java
+++ src/main/java/com/eazybytes/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.eazybytes.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +17,9 @@ public class LoginController {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    private PasswordEncoder  passwordEncoder;
+
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customer customer){
 
@@ -23,6 +27,8 @@ public class LoginController {
         ResponseEntity response= null;
 
         try{
+                String hashPwd = passwordEncoder.encode(customer.getPwd());
+                customer.setPwd(hashPwd);
                 savedCustomer = customerRepository.save(customer);
                 if(savedCustomer.getId()>0){
                     response = ResponseEntity.status(HttpStatus.CREATED).body("Used details are successfully registered");
